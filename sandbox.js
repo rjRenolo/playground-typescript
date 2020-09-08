23bc79eecdd5ac7e13d2339bdbbe58b74c62cd08
rjRenolo <rjrenolo14@gmail.com>
2020-09-08 14:37:07 +0800

    intro

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var character = "asd";
+console.log(character);
+var inputs = document.querySelectorAll("input");
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
